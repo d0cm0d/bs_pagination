b6b45f00f77800b05930d34df0fa4e9b205057f8
d0cm0d <pascal.poschenrieder@googlemail.com>
2015-04-03 07:47:21 +0200

    German language file

---
+++ localization/de.js
@@ -0,0 +1,21 @@
+/**
+ * bs_pagination simple localization - GERMAN
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
+var rsc_bs_pag = {
+    go_to_page_title: 'Gehe zu Seite',
+    rows_per_page_title: 'Reihen pro Seite',
+    current_page_label: 'Seite',
+    current_page_abbr_label: 'S.',
+    total_pages_label: 'von',
+    total_pages_abbr_label: '/',
+    total_rows_label: 'von',
+    rows_info_records: 'Eintr&auml;gen',
+    go_top_text: '&laquo;',
+    go_prev_text: '&larr;',
+    go_next_text: '&rarr;',
+    go_last_text: '&raquo;'
+};
